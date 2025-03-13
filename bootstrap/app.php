80152b09e2819e27144964c758f79fe031fc8205
ManelMostefaoui <manelmostefaoui01@gmail.com>
2025-03-13 01:57:24 +0100

    Resolved merge conflict in app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,13 +6,13 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-<<<<<<< HEAD
+
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
@@ -21,8 +21,6 @@ return Application::configure(basePath: dirname(__DIR__))
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
         ]);
 
-=======
->>>>>>> 4a915970c176a70a4c1e6235e33c4a8ca175d2d0
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
